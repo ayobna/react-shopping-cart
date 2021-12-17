1926da11750d8b02aaf6b9fe6e957e502fb594cb
ayobnas@gmail.com <ayobnas@gmail.com>
2021-12-17 16:48:42 +0200

    footer-comp

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "../../css/Footer/Footer.css"
+ function Footer() {
+    return (
+        <footer>&copy; CopyWright Ayoub Nasrallah 2021</footer>
+    )
+}
+export default Footer
