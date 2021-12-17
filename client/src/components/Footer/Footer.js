accc58755efaa3ed559d801381c0d3d0297806f6
ayobnas@gmail.com <ayobnas@gmail.com>
2021-12-17 18:01:27 +0200

    add data json

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import "../../css/Footer/Footer.css"
+import {words}from '../../words'
  function Footer() {
     return (
-        <footer>&copy; CopyWright Ayoub Nasrallah 2021</footer>
+        <footer>{words.footerTitle}</footer>
     )
 }
 export default Footer
