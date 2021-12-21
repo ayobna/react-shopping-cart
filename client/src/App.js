ceee2e02faae3bd9221918291de8700b04c00b31
ayobnas@gmail.com <ayobnas@gmail.com>
2021-12-21 22:09:12 +0200

    filter-comp

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,33 @@ import Products from "./components/Products/Products";
 import Filter from "./components/Filter/Filter";
 function App() {
   const [products, setproducts] = useState(data);
-  console.log(products);
+  const [sort, setsort] = useState("")
+  const [category, setcategory] = useState("")
+
+  const handleFilterBySize=(e)=>{
+    setcategory(e.target.value)
+  
+
+    let newProduct =data.filter(p=>p.store_category_title===e.target.value)
+    setproducts(newProduct)
+    if(e.target.value == "ALL") {
+      setproducts(data)
+    }
+  
+  }
+
+  const handleFilterByorder =(e) => {
+    console.log(e.target.value)
+  }
   return (
     <div className="layout">
       <Header />
       <main>
         <div className="wrapper">
           <Products Products={products} />
-         <Filter/>
+         <Filter  Products={data} category={category} 
+         handleFilterByorder={handleFilterByorder}
+         handleFilterBySize={handleFilterBySize}/>
         </div>
       </main>
       <Footer />
--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -1,32 +1,33 @@
-import React from 'react'
-import '../../css/Filter/Filter.css'
+import React from "react";
+import "../../css/Filter/Filter.css";
 function Filter(props) {
-    return (
-        <div className="filter-wrapper"> 
-        <h2 className="filter-title"> Filter </h2>
-        <div className="num-of-products"> Number of Products  </div>
-        <div className="filter-by-size">
-            <span>Filter</span>
-            <select  className="filter-select">
-                <option value="ALL">ALL</option>
-                <option value="XS">XS</option>
-                <option value="S">S</option>
-                <option value="M">M</option>
-                <option value="L">L</option>
-                <option value="XL">XL</option>
-                <option value="XXL">XXL</option>
-            </select>
-        </div>
-        <div className="filter-by-size">
-            <span>Order</span>
-            <select className="filter-select" >
-                <option value="latest">Latest</option>
-                <option value="lowest">lowest</option>
-                <option value="highest">Highest</option>
-            </select>
-        </div>
+  return (
+    <div className="filter-wrapper">
+      <h2 className="filter-title"> Filter </h2>
+      <div className="num-of-products"> Number of Products </div>
+      <div className="filter-by-size" onChange={props.handleFilterBySize}>
+        <span>Filter</span>
+        <select value={props.category} className="filter-select">
+        <option value="ALL">
+            ALL
+            </option>  
+          {props.Products.map((product) => (
+            <option value={product.store_category_title}>
+              {product.store_category_title}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="filter-by-size">
+        <span>Order</span>
+        <select className="filter-select">
+          <option value="latest">Latest</option>
+          <option value="lowest">lowest</option>
+          <option value="highest">Highest</option>
+        </select>
+      </div>
     </div>
-    )
+  );
 }
 
-export default Filter
+export default Filter;
